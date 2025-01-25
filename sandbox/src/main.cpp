865ec7c4a17a6b2ba884ea7c42c0aa2512be933c
Zvend <soerenganske@hotmail.de>
2025-01-25 02:32:28 +0100

    Added sandbox test

--- sandbox/src/main.cpp
+++ sandbox/src/main.cpp
@@ -1,4 +1,18 @@
+#include <ProjectZvend/Memory.hpp>
+#include <ProjectZvend/Stopwatch.hpp>
+
+
 int main()
 {
+    auto scanner = PZvend::Memory::Scanner("kernel32.dll");
+
+    PZvend::Stopwatch watch(10.0f);
+
+    auto result = scanner.FindAll("55", PZvend::Memory::RDATA);
+    auto time   = watch.GetElapsedTime();
+
+    printf("Scanner = %p\n", (void*)&scanner);
+    printf("Results = %d (Time: %f)\n", result.size(), time);
+
     return 0;
 }
